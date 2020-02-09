0ff7733f462ff6392bcfaec0443fef994fe5fb7a
ConnorM8445 <bjc_mcclelland@msn.com>
2020-02-08 20:05:46 -0700

    Pause Screen

---
+++ DRR-PauseScreen/Assets/Scripts/Pause.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseTrigger : MonoBehaviour
+{
+    public static bool pauseActive = false;
+    public static bool settingsSaved = false;
+    public GameObject pauseMenuUI;
+    public GameObject settingsMenuUI;
+    public GameObject controlsMenuUI;
+    public GameObject videoMenuUI;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseActive)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        settingsMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        pauseActive = false;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        pauseActive = true;
+    }
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main Menu");
+    }
+    public void OpenSettings()
+    {
+        pauseMenuUI.SetActive(false);
+        settingsMenuUI.SetActive(true);
+    }
+    public void Back()
+    {
+        //save settings
+        if (settingsMenuUI.activeSelf)
+        {
+            settingsMenuUI.SetActive(false);
+            pauseMenuUI.SetActive(true);
+        }
+        if (controlsMenuUI.activeSelf)
+        {
+            controlsMenuUI.SetActive(false);
+            settingsMenuUI.SetActive(true);
+        }
+        if (videoMenuUI.activeSelf)
+        {
+            videoMenuUI.SetActive(false);
+            settingsMenuUI.SetActive(true);
+        }
+    }
+    public void OpenControls()
+    {
+        settingsMenuUI.SetActive(false);
+        controlsMenuUI.SetActive(true);
+    }
+    public void OpenVideo()
+    {
+        settingsMenuUI.SetActive(false);
+        videoMenuUI.SetActive(true);
+    }
+}
---
+++ DRR-PauseScreen/Assets/Scripts/SetVolume.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public void SetMusicLevel(float sliderValue)
+    {
+        mixer.SetFloat("volumeMusic", Mathf.Log10(sliderValue) * 20);
+    }
+    public void SetMasterLevel (float sliderValue)
+    {
+        mixer.SetFloat("volumeMaster", (Mathf.Log10(sliderValue) * 20));
+    }
+}
